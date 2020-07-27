54536031545e63bfbdcc9791d2c97b490286b475
Gabriel Ramirez <gabanox@MacBook-Pro-de-Gabriel.local>
2020-07-27 15:48:11 -0500

    player

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import VideoPlayer from "./components/VideoPlayer";
 import { withAuthenticator } from "aws-amplify-react";
 import Amplify, { Auth } from "aws-amplify";
 import aws_exports from "./aws-exports";
@@ -8,7 +9,7 @@ Amplify.configure(aws_exports);
 
 class App extends Component {
   render() {
-    return <div></div>;
+    <VideoPlayer videoUrl="https://fea3c903eeea.us-east-1.playback.live-video.net/api/video/v1/us-east-1.681707848755.channel.VRX6qzjSZtK3.m3u8" />
   }
 }
 
